4865e04ae72a5074556edeabfe18633dcf7c5847
vplend <plshchkv@gmail.com>
2025-11-07 21:43:02 +0500

    Remove `standalone` output configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts');
 
 const nextConfig: NextConfig = {
-    output: 'standalone',
     reactStrictMode: true,
     assetPrefix: process.env.NODE_ENV === 'production' ? 'https://music.plshchkv.ru' : '',
 };
