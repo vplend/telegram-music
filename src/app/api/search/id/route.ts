fff95805397008c7fbfe0ee4ccc2a6e67acc0417
vplend <plshchkv@gmail.com>
2025-11-10 02:02:47 +0500

    Добавлен маршрут для поиска песни по ID с использованием Genius API

---
+++ src/app/api/search/id/route.ts
@@ -0,0 +1,43 @@
+export async function GET(req: Request) {
+    // Получаем id из параметров запроса
+    const { searchParams } = new URL(req.url);
+    const query = searchParams.get('query');
+    if (!query) {
+        return Response.json({ error: 'No query provided' }, { status: 400 });
+    }
+
+    // Токен для доступа к Genius API
+    const accessToken = process.env.GENIUS_TOKEN as string;
+    const apiUrl = `https://api.genius.com/songs/${encodeURIComponent(query)}`;
+    // Запрос к Genius API
+    const res = await fetch(apiUrl, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            Accept: 'application/json'
+        }
+    });
+
+    if (!res.ok) {
+        return Response.json({ error: 'Genius API error' }, { status: res.status });
+    }
+
+    const data = await res.json();
+    const song = data.response.song;
+    console.log(song)
+
+    if (!song || song.length === 0) {
+        return Response.json({ error: 'No song found' }, { status: 404 });
+    }
+
+    const songJSON = {
+        title: song.title.replace(/\s*\(.*?\)/g, '').trim(),
+        artist: song.artist_names.replace(/\s*\((?!Ft\.).*?\)/g, '').replace(/(Ft\.[^()]*)\([^()]*\)([^()]*)/g, '$1$2').trim(),
+        date: song.release_date_for_display,
+        youtubeUrl: song.media.filter((i: { provider: string; }) => i.provider === 'youtube')[0].url ? song.media.filter((i: { provider: string; }) => i.provider === 'youtube')[0].url : null,
+        artImage: song.song_art_image_url
+    }
+
+    console.log(songJSON)
+
+    return Response.json(songJSON);
+}
