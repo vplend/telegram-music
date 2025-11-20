b827a882a2f077c91ee8bd3f2c4a9de733ea1be2
vplend <plshchkv@gmail.com>
2025-11-20 20:51:55 +0500

    Добавлен компонент IconButton и интеграция InfoCard в SongCell для улучшения взаимодействия с пользователем и визуального оформления.

--- src/components/Search/SongCell.tsx
+++ src/components/Search/SongCell.tsx
@@ -1,8 +1,10 @@
 // SongCell.tsx
-import { Button, Cell, Image, Info, Modal, Snackbar, Spinner, Caption, Text, Title } from "@telegram-apps/telegram-ui";
+import { Button, Cell, Image, Info, Modal, Snackbar, Spinner, Caption, Text, Title, IconButton } from "@telegram-apps/telegram-ui";
 import { ModalHeader } from "@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader";
 import React, { useState } from "react";
 import { openLink } from '@telegram-apps/sdk';
+import Icon from "../Icon/Icon";
+import InfoCard from "../Info/InfoCard";
 
 type SongCellProps = {
     image?: string;
@@ -11,13 +13,12 @@ type SongCellProps = {
     url?: any;
     year?: string;
     onSend: () => Promise<void> | void;
+    id?: string
 };
 
-const SongCell: React.FC<SongCellProps> = ({ image, title, artist, url, onSend, year }) => {
+const SongCell: React.FC<SongCellProps> = ({ image, title, artist, url, onSend, year, id }) => {
     const [loading, setLoading] = useState(false);
     const [snackbarVisible, setSnackbarVisible] = useState(false);
-    const [lyrics, setLyrics] = useState<string>('');
-    const [lyricsLoading, setLyricsLoading] = useState(false);
 
     const handleClick = async () => {
         try {
@@ -30,82 +31,52 @@ const SongCell: React.FC<SongCellProps> = ({ image, title, artist, url, onSend, 
         }
     };
 
-    console.log(lyrics)
-
     return (
-        <Modal
-            header={<ModalHeader></ModalHeader>}
-            trigger={
-                <Cell
-                    before={<Image src={image} size={48} alt="cover"/>}
-                    subtitle={artist}
-                    after={
-                        <Button size="s" onClick={handleClick} disabled={loading} style={{display: 'flex'}}>
-                            {loading ? <Spinner size="s" className="spinner"/> : "Отправить"}
-                            {snackbarVisible && <Snackbar onClose={() => {}}>{title} - {artist} отправлено в телеграм</Snackbar>}
-                        </Button>
-                    }
-                >
-                    {title}
-                </Cell>
-            }
-        >
-            <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
-                <div style={{
-                    display: 'flex',
-                    width: '90%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    paddingBottom: '1rem',
-                    flexDirection: 'column'
-                }}>
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        paddingBottom: '1rem',
-                    }}>
-                        <Image size={96} src={image} alt={`${title} - ${artist}`}/>
-                        <Info
-                            type="text"
-                            style={{
-                                marginLeft: "1rem",
-                                textAlign: 'left',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Caption style={{color: 'var(--tgui--hint_color)'}}>
-                                {year}
-                            </Caption>
-                            <br/>
-                            <Text caps={true} weight="1">
-                                {title}
-                            </Text>
-                            <br/>
-                            <Caption style={{color: 'var(--tgui--hint_color)'}}>
-                                {artist}
-                            </Caption>
-                        </Info>
-                    </div>
-                    <Button
-                        mode="gray"
-                        size="s"
-                        onClick = {() => {
-                            if (openLink.isAvailable()) {
-                                openLink(url, {
-                                    tryInstantView: true,
-                                });
+        <div>
+            <Cell
+                interactiveAnimation="opacity"
+                before={<Image src={image} size={48} alt="cover" />}
+                subtitle={artist}
+                after={
+                    <div>
+                        <Modal
+                            header={<ModalHeader></ModalHeader>}
+                            trigger={
+                                <IconButton
+                                    mode="plain"
+                                    size="s"
+                                >
+                                    <Icon icon="ellipsis_vertical" width={24} height={24} />
+                                    {snackbarVisible && <Snackbar onClose={() => { }}>{title} - {artist} отправлено в телеграм</Snackbar>}
+                                </IconButton>
                             }
-                        }}
-                    >
-                        Открыть текст песни
-                    </Button>
+                        >
+                            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
 
-                </div>
-            </div>
-        </Modal>
+                                <InfoCard id={id} />
+                            </div>
+                        </Modal>
+
+                        <IconButton
+                            mode="plain"
+                            size="s"
+                        >
+                            <Icon icon="add_circle_fill" width={24} height={24} />
+                            {snackbarVisible && <Snackbar onClose={() => { }}>{title} - {artist} отправлено в телеграм</Snackbar>}
+                        </IconButton>
+                    </div>
+
+                    // <Button size="s" onClick={handleClick} disabled={loading} style={{display: 'flex'}}>
+                    //     {loading ? <Spinner size="s" className="spinner"/> : "Отправить"}
+                    //     {snackbarVisible && <Snackbar onClose={() => {}}>{title} - {artist} отправлено в телеграм</Snackbar>}
+                    // </Button>
+                }
+            >
+                {title}
+            </Cell>
+
+        </div>
+
     );
 };
 
