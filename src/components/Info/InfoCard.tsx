71394704e1f90e84cf55b63e3ad7dd36ee3ace8b
vplend <plshchkv@gmail.com>
2025-11-20 20:51:27 +0500

    Оптимизирован компонент InfoCard: добавлено свойство id, улучшена обработка загрузки данных и добавлен индикатор загрузки.

--- src/components/Info/InfoCard.tsx
+++ src/components/Info/InfoCard.tsx
@@ -1,54 +1,67 @@
 import { useEffect, useState } from "react";
 import { getSongById, Song } from "@/lib/getSongById";
-import {useLaunchParams} from "@telegram-apps/sdk-react";
-import {Card, Skeleton} from "@telegram-apps/telegram-ui";
-import {CardChip} from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardChip/CardChip";
-import {CardCell} from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardCell/CardCell";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
+import { Card, Skeleton, Spinner } from "@telegram-apps/telegram-ui";
+import { CardChip } from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardChip/CardChip";
+import { CardCell } from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardCell/CardCell";
 import React from "react";
 
-export default function InfoCard() {
+interface InfoCardProps {
+    id?: string
+}
+
+export default function InfoCard({ id }: InfoCardProps) {
     const [song, setSong] = useState<Song | null>(null);
-    const [id, setId] = useState<string>('');
+    const [startId, setStartId] = useState<string>('');
     const launchParams = useLaunchParams();
 
-    useEffect(() => {
-        if (typeof window !== "undefined" && launchParams?.tgWebAppData?.user?.id) {
-            setId(String(launchParams.tgWebAppData.start_param));
-        }
-    }, [launchParams]);
+    if (!id) {
+        console.log('нут')
+
+        useEffect(() => {
+            if (typeof window !== "undefined" && launchParams?.tgWebAppData?.user?.id) {
+                setStartId(String(launchParams.tgWebAppData.start_param));
+            }
+        }, [launchParams]);
+
+        useEffect(() => {
+            getSongById(startId).then(setSong);
+        }, [startId]);
+    }
 
     useEffect(() => {
-        getSongById(id).then(setSong);
+        getSongById(id ? id : '').then(setSong);
     }, [id]);
-
-
+    if (!song) {
+        return (
+            <Spinner size={"s"}/>
+        )
+    }
     return (
-        <Skeleton visible={!song}>
-            <div>
-                <Card type="ambient">
-                    < React.Fragment key=".0">
-                        <CardChip readOnly>
-                            {song?.date}
-                        </CardChip>
-                        <img
-                            alt="Dog"
-                            src={song?.artImage}
-                            style={{
-                                display: 'block',
-                                height: 300,
-                                objectFit: 'cover',
-                                width: 300
-                            }}
-                        />
-                        <CardCell
-                            readOnly
-                            subtitle={song?.artist}
-                        >
-                            {song?.title}
-                        </CardCell>
-                    </React.Fragment>
-                </Card>
-            </div>
-        </Skeleton>
+        <div>
+            <Card type="ambient">
+                < React.Fragment key=".0">
+                    <CardChip readOnly>
+                        {song?.date ? song?.date : 'Дата не найдена'}
+                    </CardChip>
+                    <img
+                        alt="Dog"
+                        src={song?.artImage}
+                        style={{
+                            display: 'block',
+                            height: 300,
+                            objectFit: 'cover',
+                            width: 300
+                        }}
+                    />
+                    <CardCell
+                        readOnly
+                        subtitle={song?.artist}
+                    >
+                        {song?.title}
+                    </CardCell>
+                </React.Fragment>
+            </Card>
+        </div>
     );
 }
